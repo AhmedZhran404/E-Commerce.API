069e5fcd9bfa670d5bcd97e0c4921eef50765357
AhmedZhran404 <zxcvbnmahmed13579@gmail.com>
2025-12-04 00:14:49 +0200

    Add project files.

---
+++ E-Commerce.Web/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        // GET: baseUrl/api/Product
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetById(int id)
+        {
+            return new Product
+            {
+                Id = id,
+                Name = "Test Product"
+            };
+        }
+
+        // GET: baseUrl/api/Product
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetAll()
+        {
+            return new List<Product>();
+        }
+
+        [HttpPost]
+        public ActionResult<Product> AddProduct(Product item)
+        {
+            return item;
+        }
+
+        [HttpPut]
+        public ActionResult<Product> UpdateProduct(Product item)
+        {
+            return item;
+        }
+    }
+}
---
+++ E-Commerce.Web/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce.Web
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name Is Required")]
+        [MaxLength(7 , ErrorMessage = "Max Length Is 7 char")]
+        public string Name { get; set; } = null!;
+    }
+}
