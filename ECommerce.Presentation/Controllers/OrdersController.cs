0c3820e30151ebffd4b996769f0be21f645b5cb5
AhmedZhran404 <zxcvbnmahmed13579@gmail.com>
2026-05-30 02:54:53 +0300

    add Picture

---
+++ ECommerce.Presentation/Controllers/OrdersController.cs
@@ -0,0 +1,64 @@
+﻿using ECommerce.Services.Abstraction;
+using ECommerce.Shared.CommonResposes;
+using ECommerce.Shared.OrdersDTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Presentation.Controllers
+{
+    public class OrdersController : ApiBaseControllers
+    {
+        private readonly IOrderSevice _orderSevice;
+
+        public OrdersController(IOrderSevice orderSevice)
+        {
+            _orderSevice = orderSevice;
+        }
+
+
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult<OrderToReturnDTO>> CreateOrder(OrderDTO orderDTO)
+        {
+            var result = await _orderSevice.CreateOrderAsync(orderDTO , GetEmailFromToken());
+
+            return HandleResult(result);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrderToReturnDTO>>> GetOrders()
+        {
+
+            var result = await _orderSevice.GetAllOrderAsync(GetEmailFromToken());
+
+            return HandleResult(result);
+        }
+
+
+        [Authorize]
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<OrderToReturnDTO>> GetOrder(Guid id)
+        {
+            var result = await _orderSevice.GetSpecificOrder(GetEmailFromToken() , id);
+
+            return HandleResult(result);
+
+        }
+
+        [HttpGet("deliveryMethods")]
+        public async Task<ActionResult<IEnumerable<DeliveryMethodDTO>>> GetDeliveryMethods()
+        {
+            var result = await _orderSevice.GetAllDeliveryMethodsAsync();
+            return HandleResult(result);
+        }
+
+
+
+    }
+}
